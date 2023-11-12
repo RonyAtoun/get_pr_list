e5f1a804ef66181e8c0cf1a3b30cc80da8659921
RonyAtoun <ronya@biot-med.com>
2023-11-12 21:12:12 +0200

    Multi threaded version

--- get_pr_list.py
+++ get_pr_list.py
@@ -1,4 +1,6 @@
 import argparse
+import concurrent.futures
+import sys
 
 import requests
 from requests.auth import HTTPBasicAuth
@@ -11,7 +13,7 @@ from xlwt import Workbook
 
 
 def get_pr_list():
-    args = get_arguments()    #command line: -projectName <name of bitbucket project> for example "GEN2"
+    args = get_arguments()  # command line: -projectName <name of bitbucket project> for example "GEN2"
     load_dotenv('bitBucket.env')
     # username = os.getenv('USERNAME')  ## for some reason failing to load username from .env
     username = 'Tzvika_Lifshitz'
@@ -22,7 +24,18 @@ def get_pr_list():
     finish_get_repos = time.perf_counter()
     print(f'Finished get repos in {round(finish_get_repos - start, 2)} seconds')
 
-    pull_requests_in_date_range = get_pull_requests(repo_slugs, username, password)
+    URLS = []
+    for repo in repo_slugs:
+        URLS.append(f"https://api.bitbucket.org/2.0/repositories/softimize/" + repo + "/pullrequests?state=MERGED")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        repo_result = {executor.submit(get_pull_requests, url, username, password): url for url in URLS}
+        results = concurrent.futures.wait(repo_result)
+
+        pull_requests_in_date_range = []
+        for future in results.done:
+            repo = repo_result[future]
+            pull_requests_in_date_range.append(future.result())
+
     finish_get_pull_requests = time.perf_counter()
     print(f'Finished get PRs in {round(finish_get_pull_requests - finish_get_repos, 2)} seconds')
 
@@ -33,14 +46,13 @@ def get_pr_list():
     ws = ob.add_sheet('Merged PRs', True)
 
     generate_report(pull_requests_in_date_range, ws)
-    ob.save(args.project_name+'-Merged-PR-report.xls')
+    ob.save(args.project_name + '-Merged-PR-report.xls')
 
 
 def get_repo_slugs(username, password, project_name):
     page = 1
     # Define the API URL to get all repos
-    url = "https://api.bitbucket.org/2.0/repositories/softimize?q=project.key%3D%22"+project_name+"%22"
-    #url = "https://api.bitbucket.org/2.0/repositories/softimize?q=project.key%3D%22GEN2%22"
+    url = "https://api.bitbucket.org/2.0/repositories/softimize?q=project.key%3D%22" + project_name + "%22"
     repo_slugs = []
     while url is not None:  # handle pagination by checking of there is a next page
         response = requests.get(url, auth=HTTPBasicAuth(username, password))
@@ -56,31 +68,33 @@ def get_repo_slugs(username, password, project_name):
     return repo_slugs
 
 
-def get_pull_requests(repo_slugs, username, password):
+def get_pull_requests(url, username, password):
     # Define your date range
     start_date = datetime(2023, 8, 1).timestamp()
     end_date = datetime(2023, 12, 31).timestamp()
+
     pull_requests_in_date_range = []
-    for repo in repo_slugs:
-        print("repo: " + repo)
-        url = f"https://api.bitbucket.org/2.0/repositories/softimize/" + repo + "/pullrequests?state=MERGED"
-        while url is not None:
-            response = requests.get(url, auth=(username, password))
+
+    while url is not None:
+        response = requests.get(url, auth=(username, password))
+        if response is not None and "Rate limit" not in response:
             data = response.json()
+        else:
+            sys.exit("API Rate limit exceeded. Try again in one hour")
 
-            if 'next' in data:
-                url = data['next']
-            else:
-                url = None
+        if 'next' in data:
+            url = data['next']
+        else:
+            url = None
 
-            # Filter the pull requests by date
-            if 'values' in data:
-                paginated_pull_requests_in_date_range = [
-                    pr for pr in data['values']
-                    if
-                    start_date <= datetime.strptime(pr['created_on'], '%Y-%m-%dT%H:%M:%S.%f%z').timestamp() <= end_date
-                ]
-                pull_requests_in_date_range.extend(paginated_pull_requests_in_date_range)
+        # Filter the pull requests by date
+        if 'values' in data:
+            paginated_pull_requests_in_date_range = [
+                pr for pr in data['values']
+                if
+                start_date <= datetime.strptime(pr['created_on'], '%Y-%m-%dT%H:%M:%S.%f%z').timestamp() <= end_date
+            ]
+            pull_requests_in_date_range.extend(paginated_pull_requests_in_date_range)
     return pull_requests_in_date_range
 
 
@@ -98,26 +112,30 @@ def generate_report(pr_list, ws):
     ws.write(row, 3, "Repository", bold_style_head)
 
     row = row + 1
+    for repo in pr_list:
+        for value in repo:
+            if len(value) != 0:
+                title = value['title']
+                created_on = value['created_on'].split("T")[0]
+                merge_dest = value['destination']['branch']['name']
+                repo = value['destination']['repository']['name']
+                style = reg_style
 
-    for value in pr_list:
-        if len(value) != 0:
-            title = value['title']
-            created_on = value['created_on'].split("T")[0]
-            merge_dest = value['destination']['branch']['name']
-            repo = value['destination']['repository']['name']
-            style = reg_style
+                ws.write(row, 0, title, bold_style)
+                ws.write(row, 1, created_on, style)
+                ws.write(row, 2, merge_dest, style)
+                ws.write(row, 3, repo, style)
 
-            ws.write(row, 0, title, bold_style)
-            ws.write(row, 1, created_on, style)
-            ws.write(row, 2, merge_dest, style)
-            ws.write(row, 3, repo, style)
+                row = row + 1
 
-            row = row + 1
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-projectName', dest="project_name", action="store", required=True, help='name of bitBucket project')
+    parser.add_argument('-projectName', dest="project_name", action="store", required=True,
+                        help='name of bitBucket project')
 
     return parser.parse_args()
+
+
 if __name__ == '__main__':
     get_pr_list()
